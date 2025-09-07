9279b1794606f9bc8a67453f7e9557bd1edef8c9
Sabir5911 <sabirali786692@gmail.com>
2025-09-07 16:33:08 +0500

    mvp

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -9,20 +9,18 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from '../src/sanity/env'
 import {schema} from '../src/sanity/schemaTypes'
 import {structure} from '../src/sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   schema,
   plugins: [
     structureTool({structure}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({defaultApiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION }),
   ],
 })
