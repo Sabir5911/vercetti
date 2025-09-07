6cb1b98d35690f16b3129a49dde381cbadf82f91
Sabir5911 <sabirali786692@gmail.com>
2025-09-07 17:02:57 +0500

    mvp

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -4,12 +4,12 @@ export const apiVersion =
 process.env.SANITY_STUDIO_API_VERSION || '2025-08-10'
 
 export const dataset = assertValue(
-"production" ,
+ process.env.SANITY_STUDIO_DATASET ,
   'Missing NEXT_PUBLIC_SANITY_DATASET environment variable',
 )
 
 export const projectId = assertValue(
-  "jm0fd75g" ,
+  process.env.SANITY_STUDIO_PROJECT_ID ,
   'Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable',
 )
 
